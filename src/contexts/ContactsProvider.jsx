c2afaf24723ec2efc2fb214c319d3e6746fdcc5e
Inga Maholwana <imaholwana023@student.wethinkcode.co.za>
2025-04-21 20:29:18 +0200

    can create contacts even if refresh bro will i make the ID thing be distinct

--- src/contexts/ContactsProvider.jsx
+++ src/contexts/ContactsProvider.jsx
@@ -8,7 +8,7 @@ export function useContacts() {
 
 export function ContactsProvider({children }) {
 
-  const [contacts, setContacts] = useLocalStorage([])
+  const [contacts, setContacts] = useLocalStorage('contacts',[])
   function createContact(id, name) {
     setContacts(prevContacts => {
       return [...prevContacts, {id, name}]

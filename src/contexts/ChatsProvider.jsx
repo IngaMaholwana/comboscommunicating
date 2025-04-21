09c0bdf3294c2fec366766500c9ee9ab3186110a
Inga Maholwana <imaholwana023@student.wethinkcode.co.za>
2025-04-21 22:16:23 +0200

    created the formating for the conversation

--- src/contexts/ChatsProvider.jsx
+++ src/contexts/ChatsProvider.jsx
@@ -1,15 +1,18 @@
 import React, {useContext} from 'react'
 import useLocalStorage from '../hooks/useLocalStorage'
+import { useContacts } from './ContactsProvider'
 
 const ChatsContext = React.createContext()
 
-export function useContacts() {
+export function useChats() {
   return useContext(ChatsContext)
 }
 
 export function ChatsProvider({children }) {
 
   const [chats, setChats] = useLocalStorage('chats',[])
+
+  const { contacts } = useContacts()
   
   function createChat(recipients) {
     setChats(prevChats => {
@@ -17,8 +20,24 @@ export function ChatsProvider({children }) {
     })
   }
 
+  const formattedChats = chats.map(chat => {
+    const recipients = chat.recipients.map(recipient => {
+      const contact = contacts.find(contact => {
+        return contact.id === recipient
+      })
+      const name = (contact && contact.name) || recipient
+      return { id: recipient, name }
+    })
+    return { ...chat, recipients }
+  })
+
+  const value = {
+    chats: formattedChats,
+    createChat
+  }
+
   return (
-    <ChatsContext.Provider value={{chats, createChat}}>
+    <ChatsContext.Provider value={{value}}>
       {children}
     </ChatsContext.Provider>
   )

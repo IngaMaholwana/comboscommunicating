6ca13d3d1316c251e7dab68fefe5aa610d119af2
Inga Maholwana <imaholwana023@student.wethinkcode.co.za>
2025-04-22 23:05:12 +0200

    when have time we gonna work on the group, fixing some ui and whatever else but for now it chats oh ya AND A script for starting the server 20:04 apr 22 2025 get back into this

--- server/server.jsx
+++ server/server.jsx
@@ -1,7 +1,7 @@
 const io = require('socket.io')(5000)
 
 io.on('connection',socket => {
-    const id = socket.hanshake.query.id
+    const id = socket.handshake.query.id
     socket.join(id)
 
     socket.on('send-message',({recipients,text}) => {

45795a65bc5b5fb141137dfe876930cf2dc4f60d
Inga Maholwana <imaholwana023@student.wethinkcode.co.za>
2025-04-22 16:33:42 +0200

    created the text area and ability to send in open chat

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -1,12 +1,15 @@
 import React from 'react'
-
 import Navbar from './Navbar'
+import OpenChat from './OpenChat'
+import { useChats } from '../contexts/ChatsProvider'
 
 export default function Dashboard({id}) {
-  
+  const { selectedChat } = useChats()
+
   return (
     <div className='d-flex' style={{  height: '100vh'}}>
         <Navbar id={id} />
+        { selectedChat && <OpenChat />}
 
 
     </div>
---
+++ src/components/OpenChat.jsx
@@ -0,0 +1,40 @@
+import React, { useState } from 'react'
+import { Form, InputGroup, Button } from 'react-bootstrap'
+import { useChats } from '../contexts/ChatsProvider'
+
+export default function OpenChat() {
+  const [text, setText ] = useState('')
+  const {sendMessage, selectedChat } = useChats
+  function handleSubmit(e){
+    e.preventDefault()
+
+    sendMessage(selectedChat.recipients.map(recipient => recipient.id )
+    , text)
+    setText('')
+  } 
+
+  return (
+    <div className='d-flex flex-column flex-grow-1'>
+      <div className='flex-grow-1 overflow-auto'>
+
+
+      </div>
+      <Form onSubmit={handleSubmit}>
+        <Form.Group className='m-2' >
+          <InputGroup>
+          <Form.Control 
+          as="textarea" 
+          required 
+          value={text} 
+          onChange={e => setText(e.target.value)}
+          style={{height : '75px', resize: 'none '}}
+          />
+          <Button type="submit" variant="success">Send</Button>
+          </InputGroup>
+        </Form.Group>
+      </Form>
+
+      
+    </div>
+  )
+}

bdbccb887634464a918d652f13d74d2fa24ccb79
Inga Maholwana <imaholwana023@student.wethinkcode.co.za>
2025-04-22 21:52:49 +0200

    stating thee state

--- src/contexts/SocketProvider.jsx
+++ src/contexts/SocketProvider.jsx
@@ -1,7 +1,18 @@
-import React from 'react'
+import * as socket from 'socket.io'
+import React, { useContext, useEffect, useState } from 'react'
 
-export function SocketProvider() {
-  return (
-    <div>SocketProvider</div>
+const SocketContext = React.createContext()
+
+export function useSocket(){
+    return useContext(SocketContext)
+}
+
+export function SocketProvider(id, children) {
+  const [socket, setSocket] = useState()
+  
+    return (
+    <SocketContext.Provider value={socket}>
+        {children}
+    </SocketContext.Provider>
   )
 }
